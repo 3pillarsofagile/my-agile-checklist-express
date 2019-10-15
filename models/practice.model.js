7b743bddadada9bc5f433fa746bf274f8529f682
Rizky Syaiful <rizky.syaiful@gmail.com>
2019-10-15 08:47:39 +0700

    add order to practice

--- models/practice.model.js
+++ models/practice.model.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const practiceSchema = new Schema({
+  order: { type: Number },
   slug: { type: String, required: true, unique: true },
   title: { type: String, required: true },
   description: { type: String },
--- server.js
+++ server.js
@@ -32,6 +32,7 @@ app.use('/api/users', usersRouter);
 app.post('/userid/:id', (req, res) => {
     let all_practices = [];
     Practice.find()
+    .sort( { order: 1 } )
     .then((data) => {
         all_practices = data;
         User.findById(req.params.id)
@@ -58,20 +59,23 @@ app.get('/populate-practices', (req, res) => {
     .then(() => {       // lalu isi dengan data berikut
         Practice.create([ 
             new Practice({
+                order: 1,
                 slug : "fr",
-                title : "Apakah rencana jangka panjang produk bisa berubah drastis akibat informasi baru dari pengguna (termasuk ide fitur dari internal yang sudah divalidasi pengguna",
-                description: "",
+                title : "Apakah rencana jangka panjang produk bisa berubah drastis akibat informasi baru dari pengguna (termasuk ide fitur dari internal yang sudah divalidasi pengguna?",
+                description: "apa itu pengguna?",
                 if_yes_advice: "",
                 if_no_advice: ""
             }),
             new Practice({
+                order: 2,
                 slug : "epm",
                 title: "Apakah pihak (atau pihak-pihak) yang berkuasa menentukan fitur-fitur apa saja yang akan dikembangkan*, rutin berkomunikasi langsung dengan tim pengembang?",
                 description: "Rutin di sini bisa di level harian jika diperlukan. *) Selanjutnya disebut Product Manager.",
                 if_yes_advice: "Bagus! Tingkatkan intensitas & kualitas pertemuan Product Manager dengan tim pengembang. Tingkatkan juga transparansi dari kedua-belah pihak, sedemikian sehingga: <ol><li>Product Manager makin menghargai estimasi lama waktu pengerjaan dari tim pengembang</li><li>tim pengembang memahami alasan dibalik arahan fitur-fitur dari Product Manager</li></ol> <p>Cek praktik-praktik transparansi organisasi di <a href='https://3pillarsofagile.github.io/1-fast'>Pilar 1</a> & <a href='https://3pillarsofagile.github.io/2-flexible'>Pilar 2</a>, juga praktik <a href='https://3pillarsofagile.github.io/paper-prototyping'>Paper Prototyping</a>.</p>",
-                if_no_advice: "<p>Produk tidak akan bisa bergerak tangkas melayani pengguna jika mereka &mdash; yang memberikan pekerjaan & yang mengerjakan &mdash; tidak bertemu langsung. Kenapa? Karena <em>trust</em> tidak terbangun.</p><p>Jika tidak ada <em>trust</em>, instruksi pekerjaan dari Product Manager jadi diiringi dengan ekspektasi lama pengerjaan yang sempit &mdash; karena takut tim pengembang banyak santai menunda pekerjaan. Tim pengembang sendiri kecewa karena seperti dipaksa lembur terus-terusan & karena tidak ada penjelasan atas permintaan-permintaan fitur yang aneh bin mendadak &mdash; fenomena yang cukup wajar di Waterfall karena rencana fitur dikunci di awal. Bagi proses kerja agile, yang rencana fiturnya bisa berubah drastis, jelas jadi masalah.</p> <p>Ini biasa disebabkan oleh fenomena <em>proxy Product Manager</em>. 'Proxy' karena sosok Product Manager yang rutin berkomunikasi dengan tim pengembang tidak punya otonomi untuk menentukan fitur-fitur apa yang akan dibuat. Beliau harus terlebih dahulu meminta persetujuan atasan &mdash; alias Product Manager yang sebenarnya tapi tidak mau terjun langsung ke pengguna dan tim pengambang.</p> <p>Akhir kata, solusinya jelas: pastikan ada Product Manager yang sebenarnya (baca: yang memimpin riset pengguna & punya otoritas untuk menentukan fitur-fitur serta deal-deal dengan pihak luar). Buat yang bersangkutan berkomunikasi langsung dengan tim pengembang secara intens.</p>"
+                if_no_advice: "<p>Produk tidak akan bisa bergerak tangkas melayani pengguna jika mereka &mdash; yang memberikan pekerjaan & yang mengerjakan &mdash; tidak bertemu langsung. Kenapa? Karena <em>trust</em> tidak terbangun.</p><p>Jika tidak ada <em>trust</em>, instruksi pekerjaan dari Product Manager jadi diiringi dengan ekspektasi lama pengerjaan yang sempit &mdash; karena takut tim pengembang banyak santai menunda pekerjaan. Tim pengembang sendiri kecewa karena seperti dipaksa lembur terus-terusan & karena tidak ada penjelasan atas permintaan-permintaan fitur yang aneh bin mendadak &mdash; fenomena yang cukup wajar di Waterfall karena rencana fitur dikunci di awal. Bagi proses kerja agile, yang rencana fiturnya bisa berubah drastis, jelas jadi masalah.</p> <p>Ini biasa disebabkan oleh fenomena <em>proxy Product Manager</em>. 'Proxy' karena sosok Product Manager yang rutin berkomunikasi dengan tim pengembang tidak punya otonomi untuk menentukan fitur-fitur apa yang akan dibuat. Beliau harus terlebih dahulu meminta persetujuan atasan &mdash; alias Product Manager yang sebenarnya tapi tidak bisa terjun langsung ke pengguna dan tim pengambang.</p> <p>Akhir kata, solusinya jelas: pastikan ada Product Manager yang sebenarnya (baca: yang memimpin riset pengguna & punya otoritas untuk menentukan fitur-fitur serta deal-deal dengan pihak luar). Buat yang bersangkutan berkomunikasi langsung dengan tim pengembang secara intens.</p>"
             }),
             new Practice({
+                order: 3,
                 slug : "mvp",
                 title: "Apakah menurut kamu, target-target rilis pertama produk (atau sebuah fitur besar), sering kali sudah yang paling kecil ukurannya*?",
                 description: "*) Biasa disebut MVP &mdash; minimum viable product",
@@ -79,6 +83,7 @@ app.get('/populate-practices', (req, res) => {
                 if_no_advice: "<p>Rilis pertama yang lambat meningkatkan resiko bangkrut-karena-membuat-fitur-yang-salah. Ini amat penting, apalagi di era kompetisi digital saat ini. Edukasi Product Manager & pimpinan-pimpinan terkait tentang pentingnya MVP.</p> <p>Baca MVP & studi kasus orang lain terhadap di <a href='https://3pillarsofagile.github.io/mvp'>sini</a>.</p>"
             }),
             new Practice({
+                order: 4,
                 slug : "for",
                 title: "Setelah rilis yang pertama tersebut, apakah biasanya dilanjutkan dengan rilis-rilis yang cepat & sering?",
                 description: "Umumnya lebih dari dua minggu sekali terhitung lambat. Meski industri tertentu punya standar berbeda.",
@@ -86,6 +91,7 @@ app.get('/populate-practices', (req, res) => {
                 if_no_advice: "<p>Kompetisi bukanlah alasan utama harus rilis cepat & sering. Alasan utamanya adalah ukuran asumsi. Logikanya, makin lambat dan jarang rilis, makin besar asumsi saat rilis. Pekerjaan produksi software tidaklah sama dengan produksi pabrik. Setiap fitur baru adalah asumsi, sementara barang produksi pabrik bukan. Makin besar asumsi, makin besar peluang salahnya, dan bahkan makin sulit mencari penyebab kesalahan &mdash; karena varibel yang bermain jadi makin banyak. Jika kita segera memperkecil asumsi dan merilisnya  segera langsung ke pengguna (baca: memvalidasinya), kita akan segera diarahkan ke bentuk software yang paling membahagiakan pengguna dan menghasilkan uang buat kita."
             }),
             new Practice({
+                order: 5,
                 slug : "ed",
                 title : "Apakah Product Manager menyediakan data-data empirik (baik langsung dari pengguna maupun sumber lain) sebagai dasar alasan atas fitur baru yang dia minta?",
                 description: "",
@@ -93,27 +99,31 @@ app.get('/populate-practices', (req, res) => {
                 if_no_advice: ""
             }),
             new Practice({
+                order: 6,
                 slug : "rpm",
                 title : "Apakah ada retro rutin terkait cara Product Manager (beserta timnya) dalam meriset pengguna?",
-                description: "",
+                description: "jelasin definisi retro",
                 if_yes_advice: "",
                 if_no_advice: ""
             }),
             new Practice({
+                order: 7,
                 slug : "td",
-                title : "Apakah tim pengembang cukup transparan sehingga pihak lain (Product Manager, eksekutif, pemilik, dll) bisa menilai apakah tim pengembang memberikan etos kerja & waktu kerja yang sesuai gaji mereka?",
+                title : "Apakah tim pengembang cukup transparan sehingga pihak lain (Product Manager, eksekutif, pemilik, dll) bisa menilai apakah tim pengembang memberikan (skill) etos kerja & waktu kerja yang sesuai gaji mereka?",
                 description: "",
                 if_yes_advice: "",
                 if_no_advice: ""
             }),
             new Practice({
+                order: 8,
                 slug : "rd",
                 title : "Apakah  ada retro rutin terkait cara tim pengembang mengembangkan software?",
-                description: "",
+                description: "jelasi definisi retro",
                 if_yes_advice: "",
                 if_no_advice: ""
             }),
             new Practice({
+                order: 9,
                 slug : "enr",
                 title : "Apakah tim pengembang dilobi untuk 'berkerja lebih cepat' / 'mengambil kerjaan lebih banyak' saat mengestimasi pekerjaan?",
                 description: "",
@@ -121,12 +131,17 @@ app.get('/populate-practices', (req, res) => {
                 if_no_advice: ""
             }),
             new Practice({
+                order: 10,
                 slug : "dod",
                 title : "Apakah tim pengembang punya standar kualitas kerja tertentu, sehingga bug jadi minim dan kode mereka tetap terjaga untuk mudah dipahami?",
                 description: "",
                 if_yes_advice: "",
                 if_no_advice: ""
             })
+
+            // tambahin komitmen,
+
+
         ]).then((data) => res.json(data))
         .catch(err => res.status(400).json('Error: ' + err));
     })
